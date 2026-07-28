f565bb0e8ef5f3a1ed0ba74df8266fd665898f0a
Mubeen Khan <mubkhan01@gmail.com>
2026-07-27 20:32:07 -0400

    small mistake when i added rel

--- src/App.js
+++ src/App.js
@@ -321,9 +321,9 @@ function App() {
                 <h2>Contact</h2>
                 <p><strong>Email:</strong> mubkhan01@gmail.com</p>
                 {/* <p><strong>Phone:</strong> (123) 456-7890</p> */}
-                <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/mubeen-khan-75b48511a" target="_blank" rel="noreferrer noreferrer">linkedin.com/in/mubeen-khan</a></p>
-                <p><strong>GitHub:</strong> <a href="https://github.com/Mubeenkh" target="_blank" rel="noreferrer noreferrer" >github.com/Mubeenkh</a></p>
-                <p><strong>Portfolio Github:</strong> <a href="https://github.com/Mubeenkh/react-portfolio" target='_blank' rel="noreferrer" rel="noreferrer noreferrer">github.com/Mubeenkh/react-portfolio</a></p>
+                <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/mubeen-khan-75b48511a" target="_blank" rel="noopener noreferrer">linkedin.com/in/mubeen-khan</a></p>
+                <p><strong>GitHub:</strong> <a href="https://github.com/Mubeenkh" target="_blank" rel="noopener noreferrer">github.com/Mubeenkh</a></p>
+                <p><strong>Portfolio Github:</strong> <a href="https://github.com/Mubeenkh/react-portfolio" target='_blank' rel="noopener noreferrer">github.com/Mubeenkh/react-portfolio</a></p>
             </div>
 
             {showButton && (
